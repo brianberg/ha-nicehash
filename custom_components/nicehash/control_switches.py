9eb2ba83b1ec3f16eb1e840335c74f7509d51e79
Evan Lawrence <EL.PC.TECHS@gmail.com>
2021-11-12 15:22:15 -0500

    - Device switch status now properly reflects status of mining device (there is a delay for the status to update into Home Assistant) - Added property to switches which contains the last response from the API.  This will reflect "Success!" or otherwise the error message that was provided by the API. - Added additional properties 'status' and 'device' to switches.

--- custom_components/nicehash/control_switches.py
+++ custom_components/nicehash/control_switches.py
@@ -10,7 +10,7 @@ from homeassistant.components.switch import SwitchEntity
 
 from .coordinators import MiningRigsDataUpdateCoordinator
 from .nicehash import MiningRig, MiningRigDevice, NiceHashPrivateClient
-from .const import DOMAIN
+from .const import DOMAIN, NICEHASH_ATTRIBUTION
 import asyncio
 
 class DeviceSwitch(SwitchEntity):
@@ -28,6 +28,7 @@ class DeviceSwitch(SwitchEntity):
         self._device_id = device.id
         self._device_name = device.name
         self._client = client
+        self._status = device.status
 
 
     def _get_device(self):
@@ -42,6 +43,7 @@ class DeviceSwitch(SwitchEntity):
 class GPUSwitch(DeviceSwitch):
 
     _is_on = False
+    _last_response = "N/A"
 
     @property
     def name(self):
@@ -54,15 +56,34 @@ class GPUSwitch(DeviceSwitch):
         return f"{self._device_id}:switch"
 
     @property
-    def is_on(self):               
+    def is_on(self):
+        device = self._get_device()
+        if device.status == "Mining":
+            self._is_on = True
+        elif device:
+            self._is_on = False
+        else:
+            self._is_on = "unavailable"    
         return self._is_on
+
+    @property
+    def device_state_attributes(self):
+        """Sensor device state attributes"""
+        return {
+            ATTR_ATTRIBUTION: NICEHASH_ATTRIBUTION,
+            "status": self._status,
+            "device": self._device_name,
+            "last_response": self._last_response,
+        }
 
     def turn_on(self, **kwargs):
         """Turn the switch on."""
         self._is_on = True
-        asyncio.run(self._client.toggle_device(self._device_id, "START", self._rig_id))
+        response = asyncio.run(self._client.toggle_device(self._device_id, "START", self._rig_id))        
+        self._last_response = "Success!" if response["success"] else response["message"]
 
     def turn_off(self, **kwargs):
         """Turn the switch off."""
         self._is_on = False
-        asyncio.run(self._client.toggle_device(self._device_id, "STOP", self._rig_id))
+        response = asyncio.run(self._client.toggle_device(self._device_id, "STOP", self._rig_id))        
+        self._last_response = "Success!" if response["success"] else response["message"]
